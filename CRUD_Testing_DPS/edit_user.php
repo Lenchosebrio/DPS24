331e961a5f8f1e79b1d998ab8037796e0d0774ec
Lenchosebrio <lenchosebrio@gmail.com>
2024-10-22 09:32:52 -0600

    Update edit_user.php

--- CRUD_Testing_DPS/edit_user.php
+++ CRUD_Testing_DPS/edit_user.php
@@ -3,9 +3,9 @@ include("connection.php");
 $con = connection();
 
 $id_producto = $_POST['id_producto'];
-$marca = $_POST['marca'];
-$nombre = $_POST['nombre'];
-$precio = $_POST['precio'];
+$MARCA = $_POST['MARCA'];
+$NOMBRE = $_POST['NOMBRE'];
+$COSTO = $_POST['COSTO'];
 
 
 $sql="UPDATE productos SET id_producto='$id', marca='$marca', nombre='$nombre', precio='$precio' WHERE id_producto=$id";
